86e0cbe238ab6d46c6b6a26018add587f3e0cb25
Tiffany Marroquin <tiffany.marroq@gmail.com>
2018-07-27 17:17:14 -0400

    adding template files

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<section class=" site-wrapper ">
+	<div class="four-oh-four site-wrapper-inner large-text page-header ">
+		<h1>404</h1>
+	</div>
+</section>
+</div>
+<!-- Main -->
+<?php wp_footer(); ?>
+</body>
+</html>
